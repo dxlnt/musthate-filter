21c703472bf150294e5e15d58d8d8a02b70be844
dxlnt <the.xlnt@gmail.com>
2020-12-13 14:31:46 +0530

    Add files via upload

---
+++ musthachte/script.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+   var canvas =createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+    
+}
+function takesnapshot(){
+    save('clown');
+}
